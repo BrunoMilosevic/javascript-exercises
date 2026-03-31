32806e349c655d239f2f84adbfebdb13cb57642e
BrunoMilosevic <brunomilosevic1234@gmail.com>
2026-03-31 16:33:21 +0200

    Finished exercise 07 - reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let newStr = "";
+    let loopStart = str.length - 1;
 
+    for(let i = loopStart; i >= 0; i--){
+        newStr += str.charAt(i);
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
