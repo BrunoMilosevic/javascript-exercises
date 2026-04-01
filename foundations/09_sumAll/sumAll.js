80e7617b1d541ef94805cdb4eda280f95cba5c11
BrunoMilosevic <brunomilosevic1234@gmail.com>
2026-04-01 16:25:24 +0200

    Finished exercise 09 - sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((Number.isInteger(num1) && num1 >= 0) && (Number.isInteger(num2) && num2 >= 0)){
+        let min = Math.min(num1, num2);
+        let max = Math.max(num1, num2);
+        let count = max - min + 1;
+        return (count * (min + max)) / 2;
+    }
+    return "ERROR";
+    
 
 };
 
