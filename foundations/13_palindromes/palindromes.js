bc3aa52ba17656de736b80cd9b4623360cba470f
BrunoMilosevic <brunomilosevic1234@gmail.com>
2026-04-29 10:32:15 +0200

    Finished exercise 13 - Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   newStr = str.replace(/[.,!\s]+/g, "").toLowerCase();
+   return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
