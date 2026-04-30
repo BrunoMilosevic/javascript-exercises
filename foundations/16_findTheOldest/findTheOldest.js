38c6c626bc76e20fbd02b780c396def8303ccd1b
BrunoMilosevic <brunomilosevic1234@gmail.com>
2026-04-30 16:32:55 +0200

    Finished exercise 16 - Find The Oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let sortedArray = arr.sort((a, b) =>{
+        if (a.yearOfDeath === undefined){
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if(b.yearOfDeath === undefined){
+            b.yearOfDeath = new Date().getFullYear();
+        } 
+        if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth){
+            return -1;
+        }
+        if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth){
+            return 1;
+        }
+    })
+    return sortedArray.at(-1);
 };
+console.log(findTheOldest([{
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]))
 
 // Do not edit below this line
 module.exports = findTheOldest;
